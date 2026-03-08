fff5a122145e9da36319ebf91409e4f93adb54cf
ShinSeungGyu <js88219@gmail.com>
2026-03-09 00:05:39 +0900

    Revert "env에서 api를 읽어와 코드 진행"
    This reverts commit e794fe7a99a2209365e3ab7d9bbf2b73ac2af447.

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,18 +2,11 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
-		//.env 파일을 읽고
-		Dotenv dotenv = Dotenv.configure().load();
-		// 파일 안의 key-value 쌍을 자바 시스템 변수로 등록
-		dotenv.entries().forEach( entry -> { System.setProperty(entry.getKey(), entry.getValue());
-		});
-
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
